53919ffb74cb468fe1ae26a83dcbd1df6fcb61d6
Tan Bui at home <tan.bui.home@gmail.com>
2017-08-06 18:26:29 +0300

    Add service worker to pre-load pages

---
+++ my-profile/sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-loader.js',
+    '/src/**/*',
+    '/images/*'
+  ],
+  navigateFallback: '/index.html'
+};
