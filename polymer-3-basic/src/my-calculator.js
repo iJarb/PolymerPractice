903f40b9ceb3aeb6fa28c94da87262ce475dabee
Tan Bui <tan@vaadin.com>
2018-06-24 12:02:00 +0300

    Rerender if property is mutable type

--- polymer-3-basic/src/my-calculator.js
+++ polymer-3-basic/src/my-calculator.js
@@ -1,15 +1,23 @@
+/**
+ * Mutable type Object, Array properties need to call requestRender if the value is changed,
+ *  in order to update the DOM
+ * 
+ */
 import { LitElement, html } from '@polymer/lit-element';
 
 class MyCalculator extends LitElement {
 
     constructor() {
         super();
-        this.total = 0;
+        this.total = {
+            number: 0
+        };
+
     }
 
     static get properties() { 
         return { 
-            total: Number
+            total: Object
         } 
     }
 
@@ -29,15 +37,21 @@ class MyCalculator extends LitElement {
             .sub-button { background-color: #ffaa00; }
             </style>
 
-            <div id="total" class="total">${this.total}</div>
+            <div id="total" class="total">${this.total.number}</div>
             <div id="addBtn" class="button add-button">+</div>
             <div id="subBtn" class="button sub-button">-</div>
         `;
     }
 
     _firstRendered() {
-        this._root.getElementById("addBtn").addEventListener("click", e => this.total++);
-        this._root.getElementById("subBtn").addEventListener("click", e => this.total--);
+        this._root.getElementById("addBtn").addEventListener("click", e => {
+            this.total.number++;
+            this.requestRender();
+        });
+        this._root.getElementById("subBtn").addEventListener("click", e => {
+            this.total.number--;
+            this.requestRender();
+        });
     }
 }
 
