66012bd77c0e5030664052c238ab959cfc96574d
Tan Bui <tan@vaadin.com>
2019-03-22 16:28:32 +0200

    Update bookmark card style

--- phonebook-app/src/components/BookmarkList.js
+++ phonebook-app/src/components/BookmarkList.js
@@ -1,4 +1,7 @@
 import { LitElement, html } from 'lit-element';
+import '@polymer/paper-card';
+import '@vaadin/vaadin-icons';
+import '@vaadin/vaadin-button';
 
 export default class BookmarkList extends LitElement {
 
@@ -17,32 +20,42 @@ export default class BookmarkList extends LitElement {
     window.dispatchEvent(new CustomEvent('show-form', {
       detail: {
         mode: 'edit',
-        id: e.currentTarget.id,
-        data: this.allContacts[e.currentTarget.id]
+        id: e.currentTarget.parentElement.id,
+        data: this.allContacts[e.currentTarget.parentElement.id]
       }
     }));
+  }
+
+  _callContact(e) {
+    const contact = this.allContacts[e.currentTarget.parentElement.id];
+    window.open('tel:' + contact.phone_number);
   }
 
   displayBookmarkContacts() {
     return this.allContacts.map((contact, index) => {
       if (contact.bookmark) {
         return html`
-                <div class="card" .id="${index}" @click="${this._editContact}">
-                  <div class="user-img"></div>
-                  <div class="fullname">
-                    <span class="text">${contact.first_name} ${contact.last_name}</span>
-                    <span class="sub">Full name</span>
-                  </div>
-                  <div class="number">
-                    <span class="text">${contact.phone_number}</span>
-                    <span class="sub">Phone</span>
-                  </div>
-                  <div class="number">
-                    <span class="text">${contact.email}</span>
-                    <span class="sub">Email</span>
-                  </div>
-                </div>
-                `;
+          <paper-card heading="${contact.first_name + " " + contact.last_name}" image="/assets/img/user-image.jpg">
+            <div class="card-content">
+              <vaadin-button theme="tertiary">
+                <iron-icon icon="vaadin:phone-landline" slot="prefix"></iron-icon>
+                <span class="text">${contact.phone_number}</span>
+              </vaadin-button>
+              <vaadin-button theme="tertiary">
+                <iron-icon icon="vaadin:envelope" slot="prefix"></iron-icon>
+                <span class="text">${contact.email}</span>
+              </vaadin-button>
+            </div>
+            <div class="card-actions" .id="${index}">
+              <vaadin-button @click="${this._callContact}">
+                <iron-icon icon="vaadin:phone" slot="prefix"></iron-icon>Call
+              </vaadin-button>
+              <vaadin-button @click="${this._editContact}">
+                <iron-icon icon="vaadin:edit" slot="prefix"></iron-icon>Edit
+              </vaadin-button>
+            </div>
+          </paper-card>
+          `;
       }
     });
   }
@@ -60,74 +73,21 @@ export default class BookmarkList extends LitElement {
             grid-column: 1/4;
         }
         .bookmark {
-            max-width: 800px;
             display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-columns: 1fr 1fr 1fr 1fr;
             grid-gap: 20px;
         }
-        .card {
-            width: 100%;
-            display: grid;
-            padding-top: 15px;
-            grid-template-columns: 1fr 1fr;
-            color: #3d4060;
-            border-radius: 10px;
-            transition: all .3s ease-in-out;
-            cursor: pointer;
-            -webkit-box-shadow: 0px 4px 77px -17px  rgba(0,0,0, 0.36);
-            -moz-box-shadow: 0px 4px 77px -17px  rgba(0,0,0, 0.36);
-            box-shadow: 0px 4px 77px -17px  rgba(0,0,0, 0.36);
+        .card-content {
+          text-align: center;
         }
-        .user-img {
-            background-image: url("/assets/img/user-image.jpg");
-            height: 80px;
-            width: 80px;
-            background-size: cover;
-            background-position: center center;
-            border-radius: 60px;
-            grid-column: 1/3;
-            align-self: center;
-            justify-self: center;
+        .card-actions {
+          text-align: right;
         }
-        .fullname {
-            color: #3d4060;
-            font-weight: 700;
-            text-transform: capitalize;
-            font-size: 1.3em;
-            grid-column: 1/3;
-            border-bottom: 1px solid rgba(0,0,0,.1);
-        }
-        .number {
-            grid-column: 1/3;
-            border-bottom: 1px solid rgba(0,0,0,.1);
-        }
-        .zipcode {
-            border-left: 1px solid rgba(0,0,0,.1);
-        }
-
-        .text {
-            display: block;
-            text-align: center;
-        }
-        .sub {
-            display: block;
-            color: #b4b5c5;
-            font-weight: 300;
-            font-size: .8rem;
-            text-align: center;
-            margin: 5px 0;
-        }
-        .fullname, .user-img {
-            font-size: 1.3em;
-            font-weight: 400;
-            padding: 15px;
-        }
-
       </style>
-        <section class="bookmark">
-            <h2>Bookmark</h2>
-            ${this.displayBookmarkContacts()}
-        </section>
+      <h2>Bookmark</h2>
+      <section class="bookmark">
+          ${this.displayBookmarkContacts()}
+      </section>
     `;
   }
 }
