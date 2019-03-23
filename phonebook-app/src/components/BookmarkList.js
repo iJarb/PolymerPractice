8f10658d95c77435d33340aa4c709e22cafdd085
Tan Bui <tan@vaadin.com>
2019-03-23 19:01:32 +0200

    Smaller the bookmark card

--- phonebook-app/src/components/BookmarkList.js
+++ phonebook-app/src/components/BookmarkList.js
@@ -36,16 +36,17 @@ export default class BookmarkList extends LitElement {
     return this.allContacts.map((contact, index) => {
       if (contact.bookmark) {
         return html`
-          <paper-card heading="${contact.first_name + " " + contact.last_name}" image="/assets/img/${contact.image ? contact.image : 'user-image.jpg'}">
+          <paper-card image="/assets/img/${contact.image ? contact.image : 'user-image.jpg'}">
             <div class="card-content">
-              <vaadin-button theme="tertiary">
-                <iron-icon icon="vaadin:phone-landline" slot="prefix"></iron-icon>
+              <h3>${contact.first_name + " " + contact.last_name}</h3>
+              <div>
+                <iron-icon icon="vaadin:phone-landline" slot="prefix" size="8"></iron-icon>
                 <span class="text">${contact.phone_number}</span>
-              </vaadin-button>
-              <vaadin-button theme="tertiary">
+              </div>
+              <div>
                 <iron-icon icon="vaadin:envelope" slot="prefix"></iron-icon>
                 <span class="text">${contact.email}</span>
-              </vaadin-button>
+              </div>
             </div>
             <div class="card-actions" .id="${index}">
               <vaadin-button @click="${this._callContact}">
@@ -76,12 +77,18 @@ export default class BookmarkList extends LitElement {
       css`
         :host {
           display: block;
-          --paper-card-header-color: #1676F3;
+          --main-color: #1676F3;
+          --paper-card-header-color: var(--main-color);
+          --iron-icon-width: 0.85em;
+          --iron-icon-height: 0.85em;
         }
         h2 {
-            color: #3e4162;
-            font-weight: 300;
-            grid-column: 1/4;
+          color: #3e4162;
+          font-weight: 300;
+          grid-column: 1/4;
+        }
+        h3 {
+          margin: 5px 0;
         }
         .bookmark {
           display: grid;
@@ -89,15 +96,22 @@ export default class BookmarkList extends LitElement {
           grid-gap: 20px;
         }
         paper-card {
-          max-width: 300px;
+          max-width: 250px;
         }
         .card-content {
-          text-align: center;
+          text-align: left;
+          font-size: .9em;
+          padding: 0.625em;
+          color: var(--main-color);
+        }
+        .card-content div {
+          margin: 3px 0;
+          white-space: nowrap;
         }
         .card-actions {
-            display: grid;
-            grid-template-columns: 3fr 2fr;
-            grid-gap: 5px;
+          display: grid;
+          grid-template-columns: 3fr 2fr;
+          grid-gap: 5px;
         }
         @media only screen and (max-width: 900px) {
           .bookmark {
